2a7a1ec5230b1da1ed619f4c6f126f3df8ae3112
testdemo1227 <testdemo1227@outlook.com>
2022-05-11 07:03:07 -0400

    Init

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class UserController {
+    @GetMapping
+    public static demo() {
+        return "Welcome to Github";
+    }
+}
